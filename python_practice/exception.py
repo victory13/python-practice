dd57d6cff6ac47af5a687b1ba06d9b3fdc00d5b6
victory13 <nikimalg@gmail.com>
2021-04-09 13:00:23 +0530

    Exception handlimg added

---
+++ python_practice/exception.py
@@ -0,0 +1,35 @@
+#
+# def exam(first_exam,second_exam):
+#     if first_exam < 50:
+#         raise Exception
+#     elif second_exam < 50:
+#         raise Exception
+#
+# first = int(input("Enter first exam result"))
+# second = int(input("Enter second exam result"))
+#
+# try:
+#     exam(first,second)
+# except:
+#     print("Exam result is less")
+#
+# else:
+#     print("Exam result greater than 50")
+
+""" 2nd
+try:
+    a = int(input("Enter numerator number: "))
+    b = int(input("Enter denominator number: "))
+    print("Result of division" +str(a/b))
+
+# except block handling division by zero
+except(ZeroDivisionError,ValueError):
+    print("You have divided a number by zero, which is not allowed.")
+#generic except
+except:
+    print("Something wrong?")
+finally:
+    print("That's a wrap")
+
+"""
+
